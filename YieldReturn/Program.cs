15144beff624c7bcb98f62bb5741b4081fa09bc5
Lucas <9322695+g4m3r0@users.noreply.github.com>
2023-05-13 13:51:38 +0200

    Add yield return demo

---
+++ YieldReturn/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using YieldReturn;
+
+Console.WriteLine("Hello, World!");
+ProcessPeople();
+
+
+static void ProcessPeople()
+{
+    var people = GetPeople(1_000_000);
+
+    // Only when people is being accessed GetPeople() will be called
+    // We lazily iterate over the GetPeople() method
+    // Thereby we only need to generate 1000 ppl instead of 1 million
+    foreach (var person in people)
+    {
+        if (person.Id < 1000)
+        {
+            Console.WriteLine(person.Name);
+        } else
+        {
+            break;
+        }
+    }
+}
+
+static IEnumerable<Person> GetPeople(int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        yield return new Person { Id = i,  Name = $"Person {i}" };
+    }
+}   
